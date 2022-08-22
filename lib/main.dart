b6e2adb23bf9844518cc8720117e3bf4357a75cc
Azad AS <azad45518@gmail.com>
2022-08-22 18:38:13 +0530

    First Flutter App Leraning

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+ 
+void main() {
+  runApp(MaterialApp(
+  title: "azads first app",
+  home: Scaffold(
+    body: Container(
+      child: Center(
+        child: Text("Welcome to first app development"),
+      ),
+    ),
+  ),
+));
+} 
